3a23f25490bbe7690e2afad62a0ce5e6b6064184
Nagini Kandula <nagini.kandula@gmail.com>
2026-02-18 12:02:31 +0530

    error with index so gave *

--- outouts.tf
+++ outouts.tf
@@ -1,12 +1,12 @@
 output "instance_publicip" {
     description = "my ec2 public ip"
     sensitive = false
-    value = aws_instance.myec2new22[count.index].public_ip
+    value = aws_instance.myec2new22.*.public_ip
   
 }
 output "instance_dns" {
     description = "my ec2 dns"
-        value = aws_instance.myec2new22[count.index].public_dns
+        value = aws_instance.myec2new22.*.public_dns
         sensitive = false
 
   
